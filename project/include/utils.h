3abe220bf4d70c61ec2731485ff5bcb30a4bf380
Khorosheva Anna <ershova_sg@mail.ru>
2022-03-17 20:52:22 +0300

    Выполнение первого кейса

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,8 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include<stdlib.h>
+#include<stdio.h>
 
-size_t timer_from(unsigned char from);
+size_t timer_from(int from);
 // TODO: Implement `power of` function
 // int custom_pow(int base, int power);
 
